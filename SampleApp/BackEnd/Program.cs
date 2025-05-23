3f5cffd024e951061d01ec7aa6f111562a0f3fd8
Jayharron Abejar <72720429+jaymar921@users.noreply.github.com>
2025-05-23 07:09:48 +0000

    New Endpoint, modified program.cs

--- SampleApp/BackEnd/Program.cs
+++ SampleApp/BackEnd/Program.cs
@@ -46,9 +46,49 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+// Create a new endpoint named /WeatherForecastByCity/{city}, that accepts a city name in the urls as a paremeter and generates a random forecast for that city
+app.MapGet("/WeatherForecastByCity/{city}", (string city) =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecastByCity
+        (
+            city,
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecastByCity")
+.WithOpenApi();
+
 app.Run();
 
+/// <summary>
+/// Represents a weather forecast for a specific date.
+/// </summary>
+/// <param name="Date">The date of the forecast.</param>
+/// <param name="TemperatureC">The temperature in Celsius.</param>
+/// <param name="Summary">A summary or description of the weather.</param>
+/// <remarks>
+/// Provides a computed property <see cref="TemperatureF"/> to get the temperature in Fahrenheit.
+/// </remarks>
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+/// <summary>
+/// Represents a weather forecast for a specific city and date.
+/// </summary>
+/// <param name="City">The name of the city.</param>
+/// <param name="Date">The date of the forecast.</param>
+/// <param name="TemperatureC">The temperature in Celsius.</param>
+/// <param name="Summary">A summary or description of the weather.</param>
+internal record WeatherForecastByCity(string City, DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
